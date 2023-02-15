94903c1968a8b0d6cffc34298a603edf262802d1
Felipe Rosa <felipesrosa@gmail.com>
2023-02-15 19:58:15 -0300

    feat: create user entity

---
+++ src/main/java/rosa/felipe/pdi/studies/infra/repository/data/models/UserEntity.java
@@ -0,0 +1,23 @@
+package rosa.felipe.pdi.studies.infra.repository.data.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue()
+    private Integer id;
+    private String userName;
+    private String email;
+    private String password;
+}
